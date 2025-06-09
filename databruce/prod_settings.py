93bc53b09360e7e67b674cac500ca278aba5746e
Brian <bvw20002010@gmail.com>
2025-06-09 00:50:02 +0000

    new settings

--- databruce/prod_settings.py
+++ databruce/prod_settings.py
@@ -187,8 +187,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-    },
+        "BACKEND": "django_redis.cache.RedisCache",
+	"LOCATION": "redis://127.0.0.1:6379/1",
+	"OPTIONS": {
+		"CLIENT_CLASS": "django_redis.client.DefaultClient",
+	}
+    }
 }
 
 REST_FRAMEWORK = {
