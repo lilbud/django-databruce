57683b12a81cea2a23476ffb29852647070f2f86
Brian <bvw20002010@gmail.com>
2025-07-03 17:57:54 -0400

    update static root

--- databruce/settings.py
+++ databruce/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = Path(BASE_DIR) / "static/"
 LOGIN_REDIRECT_URL = "/"
 
 # Default primary key field type
