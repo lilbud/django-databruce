a9bacb64a2dce5902ec2a9a478e56b1226e957b7
Brian <bvw20002010@gmail.com>
2026-02-11 23:08:23 -0500

    update admin, ensure all fields have autocomplete and search working on all

--- databruce/admin.py
+++ databruce/admin.py
@@ -1,162 +1,229 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin import site
+from django.contrib.auth.models import User
+from unfold.admin import ModelAdmin, StackedInline, TabularInline
 
 from . import models
 
 
-class OnstageInline(admin.TabularInline):
+class OnstageInline(TabularInline):
     model = models.Onstage
 
-    list_select_related = ["relation", "band", "event"]
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "relation",
+                "band",
+            )
+        )
+
     autocomplete_fields = ["relation", "band"]
 
-    fields = [
-        "relation",
-        "band",
-        "note",
-        "guest",
-    ]
+    fields = ["relation", "band", "note", "guest"]
     fk_name = "event"
     ordering = ("relation__name",)
     extra = 0
 
 
-class SetlistInline(admin.TabularInline):
+class SetlistInline(TabularInline):
     model = models.Setlists
-    list_select_related = ["song", "event"]
+
+    def get_queryset(self, request):
+        return (
+            super()
+            .get_queryset(request)
+            .select_related(
+                "song",
+                "event",
+            )
+        )
+
     autocomplete_fields = ["song"]
-    fields = [
-        "event",
-        "set_name",
-        "song_num",
-        "song",
-        "segue",
-        "note",
-        "premiere",
-        "debut",
-        "sign_request",
-        "instrumental",
-        "nobruce",
-    ]  # Specify the fields you want to include
+
+    fields = ["set_name", "song_num", "song", "segue", "note"]
     fk_name = "event"
     ordering = ("song_num",)
     extra = 0
 
 
-class ReleaseTrackInline(admin.TabularInline):
+class ReleaseTrackInline(TabularInline):
     model = models.ReleaseTracks
 
     def get_queryset(self, request):
         base_qs = super().get_queryset(request)
         return (
             base_qs.select_related("song", "release")
-            .prefetch_related("setlist", "event")
+            .prefetch_related("event")
             .order_by("discnum", "track")
         )
 
-    autocomplete_fields = ["song", "setlist", "event"]
-    list_select_related = ["song", "release"]
+    autocomplete_fields = ["song", "event"]
+
     fields = [
         "discnum",
         "track",
         "song",
         "event",
-        "setlist",
         "length",
         "note",
     ]
     fk_name = "release"
-    ordering = ("track",)
+    ordering = ("discnum", "track")
     extra = 0
 
 
+# Unregister the default User admin
+site.unregister(User)
+
+
+@admin.register(User)
+class UserAdmin(ModelAdmin):
+    search_fields = ["username"]
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "date_joined",
+        "last_login",
+        "is_active",
+    ]
+
+
 @admin.register(models.ArchiveLinks)
-class ArchiveAdmin(admin.ModelAdmin):
+class ArchiveAdmin(ModelAdmin):
     search_fields = ["event"]
-    list_select_related = ["event"]
-    list_display = ["id", "event", "url"]
-    list_display_links = ["id", "event"]
+    list_select_related = ["event", "event__venue", "event__venue__city"]
+    autocomplete_fields = ["event"]
+    list_display = ["id", "url"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.UserAttendedShows)
-class UserAttendedShowsAdmin(admin.ModelAdmin):
-    search_fields = ["user__username", "event__id", "event__date"]
-    list_select_related = ["user", "event"]
-    list_display = ["user__username", "event"]
-    list_display_links = ["user__username", "event"]
+class UserAttendedShowsAdmin(ModelAdmin):
+    search_fields = ["user__username", "event", "event__date"]
+    list_select_related = [
+        "user",
+        "event",
+        "event__venue",
+        "event__venue__city",
+    ]
+    list_display = ["id", "user__username", "event"]
+    autocomplete_fields = ["user", "event"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.Bands)
-class BandAdmin(admin.ModelAdmin):
+class BandAdmin(ModelAdmin):
     search_fields = ["name"]
     list_display = ["id", "name"]
     list_display_links = ["id"]
-    list_select_related = ["first", "last"]
-    ordering = ("name",)
+    autocomplete_fields = ["first_event", "last_event"]
+    list_select_related = [
+        "first_event",
+        "last_event",
+        "first_event__venue",
+        "first_event__venue__city",
+        "last_event__venue",
+        "last_event__venue__city",
+    ]
 
 
 @admin.register(models.Guests)
-class GuestAdmin(admin.ModelAdmin):
+class GuestAdmin(ModelAdmin):
     autocomplete_fields = ["setlist", "guest", "event"]
     search_fields = ["guest__name", "setlist__id"]
-    list_display = ["setlist__id", "setlist__song", "guest__name"]
-    list_select_related = ["setlist", "guest"]
+    list_display = [
+        "setlist__id",
+        "setlist__event__event_id",
+        "setlist__song",
+        "guest__name",
+    ]
+    list_select_related = ["setlist", "guest", "setlist__song"]
 
 
 @admin.register(models.Bootlegs)
-class BootlegAdmin(admin.ModelAdmin):
+class BootlegAdmin(ModelAdmin):
     search_fields = ["event", "title", "label", "source"]
-    list_select_related = ["event"]
+    list_select_related = ["event", "event__venue", "event__venue__city"]
+    autocomplete_fields = ["event", "archive"]
     list_display = ["id", "event", "title", "label", "source"]
     list_display_links = ["id", "event"]
 
 
 @admin.register(models.Cities)
-class CityAdmin(admin.ModelAdmin):
+class CityAdmin(ModelAdmin):
     search_fields = ["name"]
-    list_select_related = ["state", "country"]
-    list_display = ["id", "name", "state__name", "country__name"]
-    list_display_links = ["id", "state__name", "country__name"]
+    list_select_related = [
+        "state",
+        "country",
+        "first_event",
+        "last_event",
+        "first_event__venue",
+        "last_event__venue",
+    ]
+    autocomplete_fields = ["state", "country", "first_event", "last_event"]
+    list_display = ["id", "name", "state", "country"]
+    list_display_links = ["id", "state", "country"]
 
 
 @admin.register(models.Continents)
-class ContinentAdmin(admin.ModelAdmin):
+class ContinentAdmin(ModelAdmin):
     search_fields = ["name"]
     list_display = ["id", "name"]
     list_display_links = ["id"]
 
 
 @admin.register(models.Countries)
-class CountryAdmin(admin.ModelAdmin):
+class CountryAdmin(ModelAdmin):
     search_fields = ["name"]
     list_display = ["id", "name"]
     list_display_links = ["id"]
+    list_select_related = [
+        "first_event",
+        "last_event",
+        "first_event__venue",
+        "last_event__venue",
+    ]
+    autocomplete_fields = ["first_event", "last_event", "continent"]
 
 
 @admin.register(models.Covers)
-class CoverAdmin(admin.ModelAdmin):
+class CoverAdmin(ModelAdmin):
     search_fields = ["event"]
-    list_select_related = ["event"]
+    list_select_related = ["event", "event__venue", "event__venue__city"]
     list_display = ["id", "event", "url"]
+    autocomplete_fields = ["event"]
+
     list_display_links = ["id", "event"]
 
 
 @admin.register(models.NugsReleases)
-class NugsAdmin(admin.ModelAdmin):
+class NugsAdmin(ModelAdmin):
     search_fields = ["event"]
     autocomplete_fields = ["event"]
 
-    list_select_related = ["event"]
+    list_select_related = ["event", "event__venue", "event__venue__city"]
     list_display = ["id", "event", "url", "date"]
     list_display_links = ["id", "event"]
 
 
 @admin.register(models.Events)
-class EventAdmin(admin.ModelAdmin):
-    search_fields = [
-        "id",
-        "date",
+class EventAdmin(ModelAdmin):
+    search_fields = ["id", "date", "run__name", "leg__name"]
+    autocomplete_fields = [
+        "venue",
+        "artist",
+        "tour",
+        "run",
+        "leg",
+        "nugs_id",
+        "official_id",
     ]
-    autocomplete_fields = ["venue", "artist", "tour", "run", "leg", "nugs_id"]
 
     list_display = ["id", "date"]
     list_display_links = ["id"]
@@ -164,74 +231,102 @@ class EventAdmin(admin.ModelAdmin):
 
 
 @admin.register(models.Songs)
-class SongAdmin(admin.ModelAdmin):
+class SongAdmin(ModelAdmin):
     search_fields = ["name", "original_artist"]
+    list_select_related = ["first_event", "last_event", "album"]
+    autocomplete_fields = ["first_event", "last_event", "album"]
     list_display = ["id", "name"]
     list_display_links = ["id"]
     ordering = ("name",)
 
 
 @admin.register(models.Lyrics)
-class LyricsAdmin(admin.ModelAdmin):
+class LyricsAdmin(ModelAdmin):
     search_fields = ["song__name", "text"]
     autocomplete_fields = ["song"]
     list_display = ["id", "song__name"]
     list_display_links = ["id"]
+    list_select_related = ["song"]
     ordering = ("song__name",)
 
 
 @admin.register(models.Setlists)
-class SetlistAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["event", "song"]
-    search_fields = ["song__name", "event__id", "set_name"]
-    list_select_related = True
+class SetlistAdmin(ModelAdmin):
+    autocomplete_fields = ["event", "song", "ltp"]
+    search_fields = ["song__name", "event", "set_name"]
+    list_select_related = [
+        "song",
+        "event",
+        "event__venue",
+        "event__venue__city",
+        "ltp",
+    ]
     list_display = ["id", "event", "set_name", "song_num", "song"]
     list_display_links = ["id", "event", "song"]
 
 
 @admin.register(models.Onstage)
-class OnstageAdmin(admin.ModelAdmin):
+class OnstageAdmin(ModelAdmin):
     search_fields = ["relation__name", "band__name"]
-    list_select_related = True
-    list_display = ["id", "event", "relation__name", "band__name"]
-    list_display_links = ["id", "event", "relation__name", "band__name"]
+    list_select_related = [
+        "relation",
+        "band",
+        "event",
+        "event__venue",
+        "event__venue__city",
+    ]
+    list_display = ["id", "event__event_id", "relation__name", "band__name"]
+    list_display_links = ["id"]
+    autocomplete_fields = ["event", "relation", "band"]
     ordering = ("relation__name",)
 
 
 @admin.register(models.Relations)
-class RelationAdmin(admin.ModelAdmin):
+class RelationAdmin(ModelAdmin):
     search_fields = ["name"]
     list_display = ["id", "name"]
+    list_select_related = ["first_event", "last_event"]
+    autocomplete_fields = ["first_event", "last_event"]
     list_display_links = ["id"]
     ordering = ("name",)
 
 
+@admin.register(models.ReleaseDiscs)
+class ReleaseDiscAdmin(ModelAdmin):
+    search_fields = ["release__name"]
+    list_display = ["id", "name", "release__name"]
+    list_select_related = ["release"]
+    autocomplete_fields = ["release"]
+    list_display_links = ["id"]
+
+
 @admin.register(models.ReleaseTracks)
-class ReleaseTrackAdmin(admin.ModelAdmin):
-    search_fields = ["release__name", "song_name"]
-    list_select_related = ["release", "song"]
+class ReleaseTrackAdmin(ModelAdmin):
+    search_fields = ["release__name", "song__name"]
+    list_select_related = ["release", "song", "event", "discid", "setlist"]
     list_display = ["id", "release__name", "track", "song", "song__name"]
-    list_display_links = ["id", "release__name", "song"]
+    autocomplete_fields = ["release", "song", "event", "discid", "setlist"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.Releases)
-class ReleaseAdmin(admin.ModelAdmin):
+class ReleaseAdmin(ModelAdmin):
     def get_queryset(self, request):
         base_qs = super().get_queryset(request)
         return base_qs.prefetch_related("event")
 
     search_fields = ["name", "type"]
-    list_display = ["id", "name", "type", "date"]
+    list_display = ["id", "name", "type", "date", "mbid"]
     list_display_links = ["id"]
     autocomplete_fields = ["event"]
     inlines = [ReleaseTrackInline]
 
 
 @admin.register(models.Snippets)
-class SnippetAdmin(admin.ModelAdmin):
+class SnippetAdmin(ModelAdmin):
     search_fields = [
         "snippet__name",
-        "setlist__event__id",
+        "setlist",
         "setlist__id",
     ]
 
@@ -239,67 +334,90 @@ class SnippetAdmin(admin.ModelAdmin):
 
     list_select_related = [
         "setlist",
+        "setlist__event",
         "snippet",
         "setlist__song",
-        "setlist__event",
+        "setlist",
     ]
     list_display = [
         "id",
-        "setlist__event__id",
+        "setlist",
         "setlist__song__name",
         "snippet__name",
         "note",
     ]
     list_display_links = [
         "id",
-        "setlist__event__id",
+        "setlist",
         "setlist__song__name",
         "snippet__name",
     ]
 
 
 @admin.register(models.States)
-class StateAdmin(admin.ModelAdmin):
-    search_fields = ["name", "abbrev", "country", "first", "last"]
-    list_select_related = ["country", "first", "last"]
-    list_display = ["id", "name", "abbrev", "country", "first", "last"]
-    list_display_links = ["id", "country", "first", "last"]
+class StateAdmin(ModelAdmin):
+    search_fields = ["name", "abbrev", "country", "first_event", "last_event"]
+    list_select_related = [
+        "country",
+        "first_event",
+        "last_event",
+        "first_event__venue",
+        "last_event__venue",
+        "first_event__venue__city",
+        "last_event__venue__city",
+        "country",
+    ]
 
+    autocomplete_fields = ["country", "first_event", "last_event"]
 
-from django.db.models import Q
+    list_display = ["id", "name", "abbrev", "country", "first_event", "last_event"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.Tours)
-class TourAdmin(admin.ModelAdmin):
+class TourAdmin(ModelAdmin):
     search_fields = ["name"]
-    list_select_related = ["first", "band", "last"]
-    autocomplete_fields = ["first", "last"]
-    list_display = ["id", "name", "band", "first", "last"]
-    list_display_links = ["id", "band", "first", "last"]
-    ordering = ("name",)
+    list_select_related = [
+        "first_event",
+        "band",
+        "last_event",
+        "first_event__venue",
+        "last_event__venue",
+        "first_event__venue__city",
+        "last_event__venue__city",
+    ]
+    autocomplete_fields = ["first_event", "last_event", "band"]
+    list_display = ["id", "name", "band", "first_event", "last_event"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.TourLegs)
-class TourLegAdmin(admin.ModelAdmin):
+class TourLegAdmin(ModelAdmin):
     search_fields = ["name"]
-    list_select_related = ["first", "last", "tour"]
-    list_display = ["id", "tour", "name", "first", "last"]
-    list_display_links = ["id", "tour", "first", "last"]
-    ordering = ("name",)
+    list_select_related = [
+        "first_event",
+        "last_event",
+        "tour",
+        "first_event__venue",
+        "last_event__venue",
+        "first_event__venue__city",
+        "last_event__venue__city",
+    ]
+    list_display = ["id", "tour", "name", "first_event", "last_event"]
+    autocomplete_fields = ["first_event", "last_event", "tour"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.Venues)
-class VenueAdmin(admin.ModelAdmin):
+class VenueAdmin(ModelAdmin):
     search_fields = ["name"]
-    list_select_related = ["first", "last", "city", "state", "country"]
+    list_select_related = ["first_event", "last_event", "city"]
     list_display = [
         "id",
         "name",
-        "first",
-        "last",
+        "first_event",
+        "last_event",
         "city__name",
-        "state__name",
-        "country__name",
     ]
 
     def get_search_results(self, request, queryset, search_term):
@@ -316,38 +434,54 @@ class VenueAdmin(admin.ModelAdmin):
             queryset |= self.model.objects.filter(age=search_term_as_int)
         return queryset, may_have_duplicates
 
-    autocomplete_fields = ["city", "state"]
+    autocomplete_fields = ["city"]
 
     list_display_links = [
         "id",
-        "name",
-        "first",
-        "last",
-        "city__name",
-        "state__name",
-        "country__name",
     ]
     ordering = ("name",)
 
 
 @admin.register(models.Runs)
-class RunAdmin(admin.ModelAdmin):
-    search_fields = ["name", "band"]
-    autocomplete_fields = ["band"]
-    list_select_related = ["band", "first", "last"]
-    list_display = ["id", "name", "band", "num_shows", "first", "last"]
-    list_display_links = ["id", "band", "first", "last"]
+class RunAdmin(ModelAdmin):
+    search_fields = ["name", "band__name"]
+    autocomplete_fields = ["band", "first_event", "last_event", "venue"]
+    list_select_related = [
+        "band",
+        "first_event",
+        "last_event",
+        "venue",
+        "first_event__venue",
+        "last_event__venue",
+        "first_event__venue__city",
+        "last_event__venue__city",
+    ]
+    list_display = ["id", "name", "band", "num_shows", "first_event", "last_event"]
+    list_display_links = ["id"]
 
 
 @admin.register(models.Tags)
-class TagAdmin(admin.ModelAdmin):
+class TagAdmin(ModelAdmin):
     search_fields = ["event"]
-    list_select_related = ["id", "event"]
+    list_select_related = [
+        "event",
+        "event__venue",
+        "event__venue__city",
+    ]
+    autocomplete_fields = ["event"]
     list_display = ["id", "event", "tags"]
-    list_display_links = ["id", "event"]
+    list_display_links = ["id"]
 
 
-@admin.register(models.VenuesText)
-class VenuesTextAdmin(admin.ModelAdmin):
-    search_fields = ["formatted"]
-    list_display = ["id", "formatted"]
+@admin.register(models.Contact)
+class ContactAdmin(ModelAdmin):
+    search_fields = ["email", "subject", "message"]
+    list_display = [
+        "id",
+        "email",
+        "subject",
+        "message",
+        "is_user",
+        "created_at",
+    ]
+    list_display_links = ["id"]
