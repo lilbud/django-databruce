4c5f65fcfbd2c682b3d58fdd5b3d7794ac1bf5fc
Brian <bvw20002010@gmail.com>
2026-03-16 15:01:54 -0400

    update settings to use env for redis with password

--- databruce/config/prod.py
+++ databruce/config/prod.py
@@ -97,7 +97,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": os.getenv("REDIS_URL"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
