699a0690f4c930076f5fed99da2c48db0533a37d
Brian <bvw20002010@gmail.com>
2026-07-06 11:25:47 -0400

    Update connection max age in production config
    Increased connection max age from 60 to 600 seconds.

--- databruce/config/prod.py
+++ databruce/config/prod.py
@@ -112,7 +112,7 @@ DATABASES = {
         "PASSWORD": os.getenv("SUPABASE_PASSWORD"),
         "HOST": os.getenv("SUPABASE_HOST"),
         "PORT": "5432",
-        "CONN_MAX_AGE": 60,
+        "CONN_MAX_AGE": 600,
         "CONN_HEALTH_CHECKS": True,
         "OPTIONS": {
             "options": "-c search_path=public,extensions",
