cd0fccf5a1ec4f46bdae1fe800a89a97516635f5
Brian <bvw20002010@gmail.com>
2026-07-17 22:38:48 -0400

    fix issue with searchbuilder not loading and songs table not loading

--- databruce/static/databruce/js/searchBuilder.js
+++ databruce/static/databruce/js/searchBuilder.js
@@ -2,7 +2,7 @@ function searchBuilder(table, columns) {
     new DataTable.SearchBuilder(table, {
         liveSearch: false,
         depthLimit: 1,
-        columns: sb_columns,
+        columns: columns,
     });
 
     var container = table.searchBuilder.container();
